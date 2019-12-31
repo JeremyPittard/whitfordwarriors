13c9eda5182af1dea0ba1883a09bb2beecf09914
JeremyPittard <jeremy@jpittard.net>
2019-12-31 09:38:17 +0800

    noindex

---
+++ src/templates/event-post.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const eventsPostTemplate = ({ content, contentComponent, description, title, helmet }) => {
+    const PostContent = contentComponent || Content;
+
+    return (
+        <section className="section">
+            {helmet || ""}
+            <div className="container content">
+                <div className="columns">
+                    <div className="column is-10 is-offset-1">
+                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
+                        <p>{description}</p>
+                        <PostContent content={content} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+eventsPostTemplate.propTypes = {
+    content: PropTypes.node.isRequired,
+    contentComponent: PropTypes.func,
+    description: PropTypes.string,
+    title: PropTypes.string,
+    helmet: PropTypes.object
+};
+
+const eventsPost = ({ data }) => {
+    const { markdownRemark: post } = data;
+
+    return (
+        <Layout>
+            <eventsPostTemplate
+                content={post.html}
+                contentComponent={HTMLContent}
+                description={post.frontmatter.description}
+                helmet={
+                    <Helmet titleTemplate="%s | events">
+                        <title>{`${post.frontmatter.title}`}</title>
+                        <meta name="description" content={`${post.frontmatter.description}`} />
+                    </Helmet>
+                }
+                title={post.frontmatter.title}
+            />
+        </Layout>
+    );
+};
+
+eventsPost.propTypes = {
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.object
+    })
+};
+
+export default eventsPost;
+
+export const pageQuery = graphql`
+    query eventsPostByID($id: String!) {
+        markdownRemark(id: { eq: $id }) {
+            id
+            html
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+                description
+                type
+            }
+        }
+    }
+`;
---
+++ src/templates/news-post.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const NewsPostTemplate = ({ content, contentComponent, description, title, helmet }) => {
+    const PostContent = contentComponent || Content;
+
+    return (
+        <section className="section">
+            {helmet || ""}
+            <div className="container content">
+                <div className="columns">
+                    <div className="column is-10 is-offset-1">
+                        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
+                        <p>{description}</p>
+                        <PostContent content={content} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+NewsPostTemplate.propTypes = {
+    content: PropTypes.node.isRequired,
+    contentComponent: PropTypes.func,
+    description: PropTypes.string,
+    title: PropTypes.string,
+    helmet: PropTypes.object
+};
+
+const NewsPost = ({ data }) => {
+    const { markdownRemark: post } = data;
+
+    return (
+        <Layout>
+            <NewsPostTemplate
+                content={post.html}
+                contentComponent={HTMLContent}
+                description={post.frontmatter.description}
+                helmet={
+                    <Helmet titleTemplate="%s | News">
+                        <title>{`${post.frontmatter.title}`}</title>
+                        <meta name="description" content={`${post.frontmatter.description}`} />
+                    </Helmet>
+                }
+                title={post.frontmatter.title}
+            />
+        </Layout>
+    );
+};
+
+NewsPost.propTypes = {
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.object
+    })
+};
+
+export default NewsPost;
+
+export const pageQuery = graphql`
+    query NewsPostByID($id: String!) {
+        markdownRemark(id: { eq: $id }) {
+            id
+            html
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+                description
+                type
+            }
+        }
+    }
+`;
