7fe997d97e4c963d308501f592c82bfb42c7de1b
JeremyPittard <jeremy@jpittard.net>
2020-01-05 22:30:48 +0800

    gatsby image shitting itself

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
-import TransitionLink from "gatsby-plugin-transition-link";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 const Navbar = class extends React.Component {
--- src/components/sponsorsRoll.js
+++ src/components/sponsorsRoll.js
@@ -12,19 +12,20 @@ export const SponsorsRoll = props => {
                 posts.map(({ node: post }, index) => {
                     // let i = index + 1;
                     // let delay = i < 4 ? `${i}00` : `${i - 3}00`;
+                    console.log(post.frontmatter.logo);
                     return (
                         <>
                             <div className="is-parent column is-4" key={post.id}>
-                                <a href={post.frontmatter.website} className="tile c-tile is-child box" target="_blank">
+                                <a href={post.frontmatter.website} className="tile c-tile is-child box" target="_blank" rel="noopener noreferrer">
                                     <article>
+                                        {index + 1}
                                         <PreviewCompatibleImage
                                             imageInfo={{
-                                                image: post.frontmatter.featuredimage,
-                                                alt: ` ${post.frontmatter.title}`,
+                                                image: post.frontmatter.logo,
+                                                alt: `featured image thumbnail for post ${post.frontmatter.title}`,
                                                 class: `full-width-image`
                                             }}
                                         />
-                                        <h3>{post.frontmatter.title}</h3>
                                     </article>
                                 </a>
                             </div>
@@ -38,7 +39,7 @@ export const SponsorsRoll = props => {
 const Posts = () => {
     const data = useStaticQuery(graphql`
         query SponsorsRollQuery {
-            allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, filter: { frontmatter: { isMajor: { eq: true } } }) {
+            allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___title] }, filter: { frontmatter: { isMajor: { eq: true } } }) {
                 edges {
                     node {
                         excerpt(pruneLength: 400)
@@ -50,7 +51,6 @@ const Posts = () => {
                             title
                             templateKey
                             isMajor
-                            website
                             logo {
                                 childImageSharp {
                                     fluid(maxWidth: 200, quality: 100) {
@@ -58,6 +58,7 @@ const Posts = () => {
                                     }
                                 }
                             }
+                            website
                         }
                     }
                 }
