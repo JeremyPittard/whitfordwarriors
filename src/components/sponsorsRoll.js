0673538744df2ca31e071c3ed23b75e286b7be61
JeremyPittard <jeremy@jpittard.net>
2020-01-05 23:18:56 +0800

    some colours on homepage, update logo, sponsors section home page

--- src/components/sponsorsRoll.js
+++ src/components/sponsorsRoll.js
@@ -12,20 +12,14 @@ export const SponsorsRoll = props => {
                 posts.map(({ node: post }, index) => {
                     // let i = index + 1;
                     // let delay = i < 4 ? `${i}00` : `${i - 3}00`;
+
                     console.log(post.frontmatter.logo);
                     return (
                         <>
                             <div className="is-parent column is-4" key={post.id}>
-                                <a href={post.frontmatter.website} className="tile c-tile is-child box" target="_blank" rel="noopener noreferrer">
+                                <a href={post.frontmatter.website} className="sponsor is-child" target="_blank" rel="noopener noreferrer">
                                     <article>
-                                        {index + 1}
-                                        <PreviewCompatibleImage
-                                            imageInfo={{
-                                                image: post.frontmatter.logo,
-                                                alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                                class: `full-width-image`
-                                            }}
-                                        />
+                                        <img src={post.frontmatter.logo.childImageSharp.fixed.src} />
                                     </article>
                                 </a>
                             </div>
@@ -53,8 +47,8 @@ const Posts = () => {
                             isMajor
                             logo {
                                 childImageSharp {
-                                    fluid(maxWidth: 200, quality: 100) {
-                                        ...GatsbyImageSharpFluid
+                                    fixed(width: 150) {
+                                        ...GatsbyImageSharpFixed
                                     }
                                 }
                             }
