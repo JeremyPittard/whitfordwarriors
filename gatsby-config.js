c02acb12627593704f62b6890e925256d6a95803
JeremyPittard <jeremy@jpittard.net>
2020-01-05 20:35:39 +0800

    all of the polyfills

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         },
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-sass",
+        "gatsby-plugin-polyfill-io",
         {
             // keep as first gatsby-source-filesystem plugin for gatsby image support
             resolve: "gatsby-source-filesystem",
