af8bb621e791fe3425743a4af9dfbaedfa41dbd9
JeremyPittard <jeremy@jpittard.net>
2020-09-16 21:11:23 +0800

    hopefully fix docs

--- src/components/policyRoll.js
+++ src/components/policyRoll.js
@@ -6,14 +6,17 @@ export const PolicyRoll = props => {
     const { data } = props;
     const { edges: posts } = data.allMarkdownRemark;
 
+
     return (
         <div className="columns is-multiline policy-roll">
             {posts &&
                 posts.map(({ node: post }, index) => {
+                const thePath = post.frontmatter.document.absolutePath.replace('opt/build/repo/static/', '')
+
                     return (
                         <>
                             <div className="is-parent column is-4 is-flex justify-center" key={post.id}>
-                                <a href={post.frontmatter.document.absolutePath} className="sponsor is-child blue-text policy-card" target="_blank" rel="noopener noreferrer" download>
+                                <a href={thePath} className="sponsor is-child blue-text policy-card" target="_blank" rel="noopener noreferrer" download>
                                     <img src={document} alt={post.frontmatter.title} />
                                     <article>{post.frontmatter.title}</article>
                                 </a>
