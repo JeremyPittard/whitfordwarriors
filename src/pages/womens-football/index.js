94f4fb87bcb388a528a49260debae0d7bb0e91da
JeremyPittard <jeremy@jpittard.net>
2021-04-05 09:16:25 +0800

    update node modules solved nothing, may introduce breaking changes if done completely + small copy update

--- src/pages/womens-football/index.js
+++ src/pages/womens-football/index.js
@@ -29,14 +29,16 @@ export default class BlogIndexPage extends React.Component {
                             <div className="column is-5">
                                 <h2 className="is-size-2">Womens Footy is coming in 2021</h2>
                                 <p>
-                                    As a club we are pleased to announce our intention of submitting a womens team into the comp in 2021
-                                    <br />
-                                    We hope to hit the ground running so instead of starting a team this year we have started to do prep to make sure our club is in the best position possible to support putting teams out on the field.
-                                    <br />
-                                    to keep up to date with womens footy at the club please enter your email below. if you wish to help out, participate, coach or anything in between please feel free to contact{" "}
-                                    <a href="mailto:womensfootball@whitfordwarriors.com.au">womensfootball@whitfordwarriors.com.au</a>
+                                    Our Women’s team is getting ready for season 2021 with round kicking off on 24 April 2021 and the club is keen to shore up its playing numbers.
+                                    Entering their first season in the competition, the women have embraced every challenge and continue to build on their skills and game sense ready for round 1.
+                                    Training is on Tuesday and Thursday nights from 5.45pm at MacDonald Reserve, Padbury. <br/> <br/>
+                                    If you enjoy keeping fit, being part of a team and looking for a winter sport, then joining our Warriors women’s team could be the thing for you.
+                                    No experience required with plenty of time at training to improve and develop your skills with our experienced and dedicated coaching team.
+                                    While there is enough talent to take the field for the 2021 campaign, the club continues to welcome players to ensure there is adequate coverage for the possible absence of players through unavailability.
+                                    Anyone interested in playing or offering support in an off-field capacity should contact Brett on <a href="tel:0477775118">0477 775 118</a> or email <a href="mailto:womensfootball@whitfordwarriors.com.au">womensfootball@whitfordwarriors.com.au</a>.
+                                    Players who have never played Australian football before are just as welcome as those who have experience in the sport.
                                 </p>
-                                <div className="content womens-footy-form">
+                                    <div className="content womens-footy-form">
                                     <WomensMailchimpSubscribe />
                                 </div>
                             </div>
@@ -47,3 +49,4 @@ export default class BlogIndexPage extends React.Component {
         );
     }
 }
+
